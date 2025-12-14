7209a761176aaac9fd6ffa8badcaaba2f9016d75
Liamoiknine <l.j.oiknine@wustl.edu>
2025-12-14 17:37:51 -0600

    create modules for each task within exam

---
+++ Sources/Assessment/TranscriptItem.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+/// Represents an item in the transcript
+struct TranscriptItem: Identifiable, Equatable {
+    let id: UUID
+    let text: String
+    let timestamp: Date
+    let type: TranscriptItemType
+    var isHighlighted: Bool
+    
+    init(id: UUID = UUID(), text: String, timestamp: Date = Date(), type: TranscriptItemType, isHighlighted: Bool = false) {
+        self.id = id
+        self.text = text
+        self.timestamp = timestamp
+        self.type = type
+        self.isHighlighted = isHighlighted
+    }
+}
+
+enum TranscriptItemType {
+    case instruction
+    case word
+    case prompt
+    case trialAnnouncement
+}
+
